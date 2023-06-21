2bcd838f3841f7db8c168ba0d8b7ff4167ed03b0
jakergrossman <jake.r.grossman@gmail.com>
2023-06-21 17:20:08 -0500

    mAkE nExT cAsE bE bAsEd On PrEvIoUs AlPhA iN lInE

--- lua/sponge-bob.lua
+++ lua/sponge-bob.lua
@@ -3,7 +3,6 @@ local M = {}
 local group = vim.api.nvim_create_augroup("sponge-bob", {})
 
 local enabled = false
-local upper = false
 
 M.enable = function()
   enabled = true
@@ -18,13 +17,16 @@ M.enable = function()
           return
         end
 
-        if upper then
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local line_prefix = vim.api.nvim_buf_get_text(0, row - 1, 0, row - 1, col, {})[1]
+        local last_char = string.reverse(line_prefix):gmatch "%a"()
+        local last_was_upper = last_char and last_char:upper() == last_char
+
+        if last_was_upper then
           vim.v.char = vim.v.char:upper()
         else
           vim.v.char = vim.v.char:lower()
         end
-
-        upper = not upper
       end
     end,
   })
